319e90ed8cadc2888e2c135e5143a949fefc5253
francojcuevas <54785168+francojcuevas@users.noreply.github.com>
2022-04-13 17:24:05 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-csvFIle=$1
+csvFile=$1
 projectFile=$2
-reportFIle=$3
-cat $csvFIle
-cat $projectName
-cat $reportFile
+reportFile=$3
 #rm $reportFile
 docker run --rm -v $WORKSPACE:/workspace swethapn14/repo_perf:JmeterLatest -Jjmeterengine.stopfail.system.exit=true -Jcsvfile=/workspace/$csvFile -n -t /workspace/$projectName -l /workspace/$reportFile
 java -jar jmeter-junit-xml-converter-0.0.1-SNAPSHOT-jar-with-dependencies.jar reportejenkins.jtl reportejenkinsconvertido.xml
